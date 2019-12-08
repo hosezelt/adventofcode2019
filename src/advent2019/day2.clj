b89512c22fda59263ceaa5aa0236a45f5abef6a1
Mauro <saf_moro@hotmail.com>
2019-12-08 17:59:16 +0100

    Day2 Refactor

--- src/advent2019/day2.clj
+++ src/advent2019/day2.clj
@@ -5,30 +5,30 @@
 (defn split-input [input]
     (s/split input #","))
 
-(def instack (assoc (assoc (into [] (map read-string (split-input (utils/day-file 2)))) 1 12) 2 2))
+(defn instack [noun verb] (assoc (assoc (into [] (map read-string (split-input (utils/day-file 2)))) 1 noun) 2 verb))
 
-(defn instack2 [noun verb] (assoc (assoc (into [] (map read-string (split-input (utils/day-file 2)))) 1 noun) 2 verb))
-
-(defn operation [stack count]
-(let [instr-set (take 4 (drop count stack))]
-    (let [[opcode 
-           first-param
-           second-param
-           loc] instr-set]
-    (cond (= 99 opcode) (reduced stack)
-          (= 1 opcode) (assoc stack loc (+ (nth stack first-param) (nth stack second-param)))
-          (= 2 opcode) (assoc stack loc (* (nth stack first-param) (nth stack second-param)))
-          ))))
-
+(defn operation [[stack itr-ptr]] (let [opcode (nth stack itr-ptr)
+    first-param (nth stack (inc itr-ptr))
+    second-param (nth stack (+ itr-ptr 2))
+    loc (nth stack (+ itr-ptr 3))]
+    (cond (= 99 opcode) [(get stack 0) "END"]
+          (= 1 opcode) [(assoc stack loc (+ (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+          (= 2 opcode) [(assoc stack loc (* (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+          )))
+   
 (defn compute [noun verb]
-    (first (reduce #(operation %1 %2) (instack2 noun verb) (range 0 (count instack) 4))))
+    (loop [[stack itr-ptr] [(instack noun verb) 0]]
+        (if (= itr-ptr "END")
+        stack
+        (recur (operation [stack itr-ptr])
+        ))))
 
 (def variable-input 
     (let [execs (for [noun (range 100) verb (range 100)] 
         {:noun noun :verb verb :result (compute noun verb)})
         exec (first (filter #(= 19690720 (:result %)) execs))] 
         (+ (* 100 (:noun exec)) (:verb exec))))
-
+    
 (defn run []
     {:part1 (compute 12 2)
     :part2 variable-input})
--- src/advent2019/day5.clj
+++ src/advent2019/day5.clj
@@ -7,3 +7,25 @@
 
 (defn split-input [input]
     (s/split input #","))
+
+(def opcodes {
+
+})
+
+(defn operation [[stack itr-ptr]] (let [opcode (nth stack itr-ptr)
+    first-param (nth stack (inc itr-ptr))
+    second-param (nth stack (+ itr-ptr 2))
+    loc (nth stack (+ itr-ptr 3))]
+    (cond (= 99 opcode) [(get stack 0) "END"]
+          (= 1 opcode) [(assoc stack loc (+ (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+          (= 2 opcode) [(assoc stack loc (* (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+          )))
+
+(defn testm [x y] (+ x y))
+ 
+(defn compute [noun verb]
+    (loop [[stack itr-ptr] [(instack2 noun verb) 0]]
+        (if (= itr-ptr "END")
+        (println stack)
+        (recur (operation [stack itr-ptr])
+        ))))
