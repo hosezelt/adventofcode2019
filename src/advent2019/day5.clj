a9b1a319c8d62c0f209fdf83f9e2cd9242c40a4f
Mauro <saf_moro@hotmail.com>
2019-12-09 08:04:50 +0100

    Day5 Initial commit

--- src/advent2019/day5.clj
+++ src/advent2019/day5.clj
@@ -2,29 +2,41 @@
     (:require [clojure.string :as s])
     (:require [advent2019.utils :as utils]))
 
-
 (def INPUT (utils/day-file 5))
 
 (defn split-input [input]
     (s/split input #","))
 
-(def opcodes {
+(defn instack []
+    (->> INPUT
+        split-input
+        (map read-string)
+        (into [])))
 
-})
+(def in-out (atom 1))
 
-(defn operation [[stack itr-ptr]] (let [opcode (nth stack itr-ptr)
-    first-param (nth stack (inc itr-ptr))
-    second-param (nth stack (+ itr-ptr 2))
-    loc (nth stack (+ itr-ptr 3))]
-    (cond (= 99 opcode) [(get stack 0) "END"]
-          (= 1 opcode) [(assoc stack loc (+ (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
-          (= 2 opcode) [(assoc stack loc (* (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
-          )))
+(defn operation [[stack itr-ptr]] 
+    (do (println itr-ptr))
+    (let [opcode (nth stack itr-ptr)
+        first-param (nth stack (inc itr-ptr) nil)
+        second-param (nth stack (+ itr-ptr 2) nil)
+        loc (nth stack (+ itr-ptr 3) nil)]
+        (cond (= 99 opcode) [stack "END"]
+            (= 1 opcode) [(assoc stack loc (+ (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+            (= 2 opcode) [(assoc stack loc (* (nth stack first-param) (nth stack second-param))) (+ itr-ptr 4)]
+            (= 3 opcode) [(do (assoc stack first-param @in-out) stack) (+ itr-ptr 2)]
+            (= 4 opcode) [(do (reset! in-out (nth stack first-param)) stack) (+ itr-ptr 2)]
+            (= 1101 opcode) [(assoc stack loc (+ first-param second-param)) (+ itr-ptr 4)]
+            (= 0101 opcode) [(assoc stack loc (+ first-param (nth stack second-param))) (+ itr-ptr 4)]
+            (= 1001 opcode) [(assoc stack loc (+ (nth stack first-param) second-param)) (+ itr-ptr 4)]
+            (= 1102 opcode) [(assoc stack loc (* first-param second-param)) (+ itr-ptr 4)]
+            (= 0102 opcode) [(assoc stack loc (* first-param (nth stack second-param))) (+ itr-ptr 4)]
+            (= 1002 opcode) [(assoc stack loc (* (nth stack first-param) second-param)) (+ itr-ptr 4)]
+            (= 104 opcode) [(do (reset! in-out first-param) stack) (+ itr-ptr 2)]
+            )))
 
-(defn testm [x y] (+ x y))
- 
-(defn compute [noun verb]
-    (loop [[stack itr-ptr] [(instack2 noun verb) 0]]
+(defn compute []
+    (loop [[stack itr-ptr] [(instack) 0]]
         (if (= itr-ptr "END")
         (println stack)
         (recur (operation [stack itr-ptr])
--- src/advent2019/utils.clj
+++ src/advent2019/utils.clj
@@ -2,9 +2,15 @@
     (:require [clojure.java.io :as io]
               [clojure.string :as str]))
   
-  (defn day-file
-    ([day] (day-file day nil))
-    ([day part]
-     (->> (io/resource (str "day" day (if part (str "-" part)) ".txt"))
-          (slurp))))
+(defn day-file
+  ([day] (day-file day nil))
+  ([day part]
+  (->> (io/resource (str "day" day (if part (str "-" part)) ".txt"))
+    (slurp))))
           
+(defn digits [n]
+  (->> n
+  (iterate #(quot % 10))
+  (take-while pos?)
+  (mapv #(mod % 10))
+  rseq))
