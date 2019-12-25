593c96908e83b677121a40c2d5194daa066dfe32
Mauro <saf_moro@hotmail.com>
2019-12-25 17:17:59 +0100

    Day7 Part1

--- src/advent2019/day7.clj
+++ src/advent2019/day7.clj
@@ -1,30 +1,33 @@
 (ns advent2019.day7
-  (:require [clojure.string :as s]
+  (:require [clojure.core.async :as a]
+            [clojure.math.combinatorics :as combo]
             [advent2019.day5 :as day5]
             [advent2019.utils :as utils]))
 
 (def INPUT (utils/day-file 7))
 
-(defn split-input [input]
-  (s/split input #","))
-
-(defn int-codes [string]
-  (->> string
-       split-input
-       (map read-string)
-       (into [])))
-
-
-
-(defn solve []
-  (let [input (int-codes INPUT)
-        in    (atom [5])
-        out   (atom [])
-        in-f  (fn [] (first @in))
-        out-f #(swap! out conj %)]
-    (day5/compute {:program input :in-f in-f :out-f out-f :pointer 0})
-    (->> @out
+(defn system-1 [input phases]
+  (let [[a b c d e :as channels] (repeatedly #(a/chan))
+        out                      (a/chan)]
+    (a/go
+      (dorun (map (fn [channel phase] (a/>!! channel phase)) channels phases))
+      (a/>!! a 0))
+    (dorun
+     (map (fn [in-chan out-chan]
+            (a/go (day5/compute
+                   {:program input :in-f #(a/<!! in-chan) :out-f #(a/>!! out-chan %) :pointer 0})
+                  (a/close! in-chan)
+                  (a/close! out-chan)))
+          [a b c d e]
+          [b c d e out]))
+    (->> (repeatedly #(a/<!! out))
+         (take-while identity)
          last)))
 
-(solve-1)
+(defn solve-1 []
+  (let [input (day5/int-codes INPUT)]
+    (->> (combo/permutations [0 1 2 3 4])
+         (map #(system-1 input %))
+         (sort >)
+         first)))
 
